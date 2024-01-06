b0ca1ad66dedd425e160dbbe2250b655c2bc5b8b
Jan Vollmer <jan@vllmr.dev>
2024-01-06 02:09:54 +0100

    remove redundant pdm install in 2.0 tests

--- noxfile.py
+++ noxfile.py
@@ -7,13 +7,13 @@ os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
 
 @nox.session
 def tests(session: nox.Session):
-    session.run_always("pdm", "install", external=True)
+    session.run_always("pdm", "install", "--no-lock", external=True)
     session.run("pytest")
 
 
 @nox.session
 def tests_20(session: nox.Session):
-    session.run_always("pdm", "install", external=True)
+    session.run_always("pdm", "install", "--no-lock", external=True)
     session.install("sqlalchemy>2.0")
     session.run("pytest")
     session.install("sqlalchemy<2.0")
