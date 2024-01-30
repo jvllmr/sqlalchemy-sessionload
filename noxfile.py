c2da61441ace18cd27376e5fa00224f551534362
Jan Vollmer <jan@vllmr.dev>
2024-01-30 22:37:11 +0100

    define pytest args in noxfile

--- noxfile.py
+++ noxfile.py
@@ -4,17 +4,24 @@ import nox
 
 os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
 
+lib_path = "src/sqlalchemy_sessionload"
+coverage_path = "coverage"
+
 
 @nox.session
 def tests(session: nox.Session):
     session.run_always("pdm", "install", "--no-lock", external=True)
-    session.run("pytest")
+    session.run(
+        "pytest", f"--cov={lib_path}", f"--cov-report=xml:{coverage_path}/coverage.xml"
+    )
 
 
 @nox.session
 def tests_20(session: nox.Session):
     session.run_always("pdm", "install", "--no-lock", external=True)
     session.install("sqlalchemy>2.0")
-    session.run("pdm", "coverage", external=True)
-    session.run("mv", "coverage.xml", "coverage_20.xml", external=True)
-    session.install("sqlalchemy<2.0")
+    session.run(
+        "pytest",
+        f"--cov={lib_path}",
+        f"--cov-report=xml:{coverage_path}/coverage_20.xml",
+    )
